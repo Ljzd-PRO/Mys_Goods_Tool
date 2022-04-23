04d3e2b0afd39acc1d0902ae6ffad26b37c93e8b
Ljzd-PRO <63289359+Ljzd-PRO@users.noreply.github.com>
2022-04-23 21:58:47 +0800

    增加分析获取Cookies的功能

--- tool.py
+++ tool.py
@@ -6,7 +6,14 @@ import json
 import platform
 import configparser
 
-def clear():
+# 所需要的Cookie
+COOKIES_NEEDED = [
+    "stuid", "stoken", "ltoken", "ltuid", "account_id", "cookie_token",
+    "login_ticket"
+]
+
+
+def clear() -> None:
     """
     清屏
     """
@@ -20,7 +27,8 @@ def clear():
     else:
         pass
 
-def goodTool():
+
+def goodTool() -> None:
     while True:
         print("> 请选择要查看的游戏：")
         print("-- 1. 崩坏3")
@@ -46,7 +54,7 @@ def goodTool():
         elif choice == "0":
             return
         else:
-            print("> 输入有误，请重新输入(回车以继续)\n")
+            print("> 输入有误，请重新输入(回车以返回)\n")
             input()
             clear()
             continue
@@ -89,13 +97,15 @@ def goodTool():
             elif good["type"] == 1:
                 print("兑换时间：{0}".format(
                     time.strftime("%Y-%m-%d %H:%M:%S",
-                                time.localtime(good["next_time"]))))
+                                  time.localtime(good["next_time"]))))
                 print("库存：{0} 件".format(good["next_num"]))
 
             if good["account_cycle_type"] == "forever":
-                print("限购：每人限购 {0}/{1}".format(good["account_exchange_num"], good["account_cycle_limit"]))
+                print("限购：每人限购 {0}/{1}".format(good["account_exchange_num"],
+                                               good["account_cycle_limit"]))
             elif good["account_cycle_type"] == "month":
-                print("限购：本月限购 {0}/{1}".format(good["account_exchange_num"], good["account_cycle_limit"]))
+                print("限购：本月限购 {0}/{1}".format(good["account_exchange_num"],
+                                               good["account_cycle_limit"]))
 
             print("预览图：{0}".format(good["icon"]))
 
@@ -103,29 +113,51 @@ def goodTool():
         input()
         clear()
 
-def addressTool():
+
+def addressTool() -> None:
     conf = configparser.RawConfigParser()
-    conf.read("config.ini", encoding = "utf-8")
-    cookie = conf.get("Config", "Cookie")
+    try:
+        conf.read("config.ini", encoding="utf-8")
+        cookie = conf.get("Config", "Cookie")
+    except:
+        print("> 读取Cookie失败，请检查你的配置文件是否正确配置。(回车以返回)")
+        input()
+        clear()
+        return
+
     headers = {
-            "Host": "api-takumi.mihoyo.com",
-            "Accept": "application/json, text/plain, */*",
-            "Origin": "https://user.mihoyo.com",
-            "Cookie": cookie,
-            "Connection": "keep-alive",
-            "x-rpc-device_id": "".join(random.sample('abcdefghijklmnopqrstuvwxyz0123456789', 32)).upper(),
-            "x-rpc-client_type": "5",
-            "User-Agent": "Mozilla/5.0 (iPhone; CPU iPhone OS 15_4 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) miHoYoBBS/2.25.1",
-            "Referer": "https://user.mihoyo.com/",
-            "Accept-Language": "zh-CN,zh-Hans;q=0.9",
-            "Accept-Encoding": "gzip, deflate, br"
-        }
-    url = "https://api-takumi.mihoyo.com/account/address/list?t={time_now}".format(time_now=round(time.time() * 1000))
+        "Host":
+        "api-takumi.mihoyo.com",
+        "Accept":
+        "application/json, text/plain, */*",
+        "Origin":
+        "https://user.mihoyo.com",
+        "Cookie":
+        cookie,
+        "Connection":
+        "keep-alive",
+        "x-rpc-device_id":
+        "".join(random.sample('abcdefghijklmnopqrstuvwxyz0123456789',
+                              32)).upper(),
+        "x-rpc-client_type":
+        "5",
+        "User-Agent":
+        "Mozilla/5.0 (iPhone; CPU iPhone OS 15_4 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) miHoYoBBS/2.25.1",
+        "Referer":
+        "https://user.mihoyo.com/",
+        "Accept-Language":
+        "zh-CN,zh-Hans;q=0.9",
+        "Accept-Encoding":
+        "gzip, deflate, br"
+    }
+    url = "https://api-takumi.mihoyo.com/account/address/list?t={time_now}".format(
+        time_now=round(time.time() * 1000))
     retry_times = 0
 
     while True:
         try:
-            address_list = json.loads(requests.get(url, headers=headers).text)["data"]["list"]
+            address_list = json.loads(requests.get(
+                url, headers=headers).text)["data"]["list"]
             break
         except:
             retry_times += 1
@@ -140,7 +172,8 @@ def addressTool():
             print("市：{0}".format(address["city_name"]))
             print("区/县：{0}".format(address["county_name"]))
             print("详细地址：{0}".format(address["addr_ext"]))
-            print("联系电话：{0}".format(address["connect_areacode"] + " " + address["connect_mobile"]))
+            print("联系电话：{0}".format(address["connect_areacode"] + " " +
+                                    address["connect_mobile"]))
             print("联系人：{0}".format(address["connect_name"]))
             print("地址ID(Address_ID)：{0}".format(address["id"]))
 
@@ -155,14 +188,15 @@ def addressTool():
                 break
             int(choice)
         except:
-            print("> 输入有误，请重新输入(回车以继续)\n")
+            print("> 输入有误，请重新输入(回车以返回)\n")
             input()
             clear()
             continue
 
         try:
             conf.set("Config", "Address_ID", choice)
-            conf.write(open("config.ini", "w"))
+            with open("config.ini", "w") as config_file:
+                conf.write(config_file)
             print("> 配置文件写入成功(回车以返回功能选择界面)")
             input()
             break
@@ -171,11 +205,120 @@ def addressTool():
             input()
             clear()
 
+
+def cookieTool() -> None:
+    print("> 请输入米游社App的抓包数据文件路径(HAR格式)：")
+    print("-- 可以直接拖动文件到此处，快速填入路径")
+    print("\n> ", end="")
+    har_path = input()
+    try:
+        har_file = open(har_path, "r")
+    except FileNotFoundError:
+        print("> 打开文件失败，尝试去除两边的无关符号(1)")
+        try:
+            har_file = open(har_path.strip("'"), "r")
+        except FileNotFoundError:
+            print("> 打开文件失败，尝试去除两边的无关符号(2)")
+            try:
+                har_file = open(har_path.strip("\""), "r")
+            except FileNotFoundError:
+                print("> 打开文件失败，尝试去除两边的无关符号(3)")
+                try:
+                    har_file = open(har_path.strip(), "r")
+                except FileNotFoundError:
+                    print("> 打开文件失败！请检查权限以及指定路径是否存在文件(回车以返回)。")
+                    input()
+                    clear()
+                    return
+    try:
+        har_data = json.load(har_file)
+    except:
+        print("> 文件错误或损坏，要求HAR文件(回车以返回)")
+        input()
+        clear()
+        return
+    har_file.close()
+
+    print("> 开始分析抓包数据")
+    cookies = {}
+    try:
+        har_data = har_data["log"]["entries"]
+        for data in har_data:
+            for cookie in data["request"]["cookies"]:
+                if len(cookies) == len(COOKIES_NEEDED):
+                    break
+                if cookie["name"] in COOKIES_NEEDED:
+                    cookies.setdefault(cookie["name"], cookie["value"])
+            if len(cookies) == len(COOKIES_NEEDED):
+                break
+    except:
+        print("> 文件错误或损坏，要求HAR文件(回车以返回)")
+        input()
+        clear()
+        return
+
+    clear()
+    print("\n> 分析完毕，结果如下：")
+    print("----------")
+    for key in COOKIES_NEEDED:
+        if key not in cookies:
+            value = "未找到"
+        else:
+            value = cookies[key]
+        print("{0}: {1}".format(key, value))
+    print("----------")
+
+    while True:
+        print("\n> 是否将分析得到的Cookies数据(包括stoken)写入配置文件？")
+        print("-- 输入 y 并回车以确认")
+        print("-- 注意：将对 config.ini 配置文件进行写入，文件中的注释和排版将被重置")
+        print("-- 注意：只会增加配置文件中Cookies没有的项，已有的项不会被覆盖更新")
+        print("-- 按回车键跳过并返回功能选择界面")
+        print("\n> ", end="")
+        choice = input()
+        clear()
+
+        if choice == "y":
+            try:
+                conf = configparser.RawConfigParser()
+                conf.read("config.ini", encoding="utf-8")
+                current_cookies = conf.get("Config", "Cookie").replace(
+                    " ", "").strip("\"").strip("'")
+
+                for key in cookies:
+                    if key + "=...;" in current_cookies:
+                        current_cookies = current_cookies.replace(
+                            key + "=...;", key + "=" + cookies[key] + ";")
+                    if key + "=" not in current_cookies:
+                        current_cookies += (key + "=" + cookies[key] + ";")
+
+                conf.set("Config", "Cookie", "\"" + current_cookies + "\"")
+                if "stoken" in cookies and conf.get("Config",
+                                                    "stoken") != None:
+                    conf.set("Config", "stoken", cookies["stoken"])
+                with open("config.ini", "w") as config_file:
+                    conf.write(config_file)
+
+                print("> 配置文件写入成功(回车以返回功能选择界面)")
+            except:
+                print("> 配置文件写入失败，检查config.ini是否存在且有权限读写(回车以返回)")
+        elif choice != "":
+            print("> 输入有误，请重新输入(回车以返回)\n")
+            input()
+            clear()
+            continue
+
+        input()
+        clear()
+        return
+
+
 while __name__ == '__main__':
     clear()
     print("> 选择功能：")
     print("-- 1. 查询商品ID(Good_ID)")
     print("-- 2. 查询送货地址ID(Address_ID)")
+    print("-- 3. 从HAR文件分析获取Cookies(包括stoken)")
     print("\n-- 0. 退出")
     print("\n> ", end="")
 
@@ -185,8 +328,10 @@ while __name__ == '__main__':
         goodTool()
     elif choice == "2":
         addressTool()
+    elif choice == "3":
+        cookieTool()
     elif choice == "0":
-        exit(0)
+        break
     else:
-        print("> 输入有误，请重新输入(回车以继续)\n")
+        print("> 输入有误，请重新输入(回车以返回)\n")
         input()
