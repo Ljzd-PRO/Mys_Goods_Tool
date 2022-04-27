5d9a6c8730df20bd4b3c674649934b45edbbea70
Ljzd-PRO <63289359+Ljzd-PRO@users.noreply.github.com>
2022-04-27 18:32:52 +0800

    修复无法获取游戏账户信息的问题、调整代码、日志增加输出错误详细信息
    将获取游戏账户信息的方式由 https://api-takumi.mihoyo.com/binding/api/getUserGameRolesByStoken 改为先获取ActionTicket再获取游戏账户信息，并在Headers中加入DS以确保能够获取ActionTicket。

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+import hashlib
 import json
 import random
+import traceback
 import requests
 import configparser
 import os
@@ -10,6 +12,16 @@ from ping3 import ping
 
 # 网络请求的超时时间（商品和游戏账户详细信息查询）
 TIME_OUT = 5
+# 兑换商品时 Headers 所用的 User-Agent
+USER_AGENT_EXCHANGE = "Mozilla/5.0 (iPhone; CPU iPhone OS 15_1 like Mac OS X) AppleWebKit/605.1.15 (KHtimeL, like Gecko) miHoYoBBS/2.14.1"
+# 获取用户 ActionTicket 时Headers所用的 User-Agent
+USER_AGENT_GET_ACTION_TICKET = "Hyperion/177 CFNetwork/1331.0.7 Darwin/21.4.0"
+# Headers所用的 x-rpc-device_model
+X_RPC_DEVICE_MODEL = "iPhone10,2"
+# Headers所用的 x-rpc-app_version
+X_RPC_APP_VERSION = "2.14.1"
+# Headers所用的 x-rpc-sys_version
+X_RPC_SYS_VERSION = "15.1"
 
 
 def clear() -> None:
@@ -27,21 +39,20 @@ def clear() -> None:
         pass
 
 
-## 获取程序运行目录绝对路径
 def get_file_path(file_name: str = "") -> str:
     """
     获取文件绝对路径, 防止在某些情况下报错
-    file_name: 文件名
+    >>> file_name: str # 文件名
     """
     return os.path.join(os.path.split(sys.argv[0])[0], file_name)
 
 
-## 储存日志
 def to_log(info_type: str = "", title: str = "", info: str = "") -> str:
     """
-    info_type: 日志的等级
-    title: 日志的标题
-    info: 日志的信息
+    储存日志
+    >>> info_type: str # 日志的等级
+    >>> title: str # 日志的标题
+    >>> info: str # 日志的信息
     """
     if not os.path.exists(get_file_path("logs")):
         os.mkdir(get_file_path("logs/"))
@@ -58,25 +69,59 @@ conf = configparser.RawConfigParser()
 conf.read(get_file_path("config.ini"), encoding="utf-8")
 
 
-## 商品兑换相关
 class Good:
+    """
+    商品兑换相关
+    """
     global conf
     cookie = conf.get("Config", "Cookie").strip("\"").strip("'")
     stoken = conf.get("Config", "stoken")
     address = conf.get("Config", "Address_ID")
     uid = conf.get("Config", "UID")
 
-    if stoken == "...":
-        stoken = ""
-    else:
+    if stoken != "..." and "stoken" not in cookie:
         cookie += ("stoken=" + stoken + ";")
+    elif "stoken" in cookie:
+        stoken = cookie.replace("=", "").replace(
+            " ", "").split("stoken")[1].split(";")[0]
+
+    bbs_uid = ""
+    if "ltuid" in cookie:
+        bbs_uid = cookie.replace("=", "").replace(
+            " ", "").split("ltuid")[1].split(";")[0]
+    elif "account_id" in cookie:
+        bbs_uid = cookie.replace("=", "").replace(
+            " ", "").split("account_id")[1].split(";")[0]
+    elif "stuid" in cookie:
+        bbs_uid = cookie.replace("=", "").replace(
+            " ", "").split("stuid")[1].split(";")[0]
+
+    def get_DS():
+        """
+        获取Headers中所需DS
+        """
+        ## DS 加密算法:
+        # 1. https://github.com/lhllhx/miyoubi/issues/3
+        # 2. https://github.com/jianggaocheng/mihoyo-signin/blob/master/lib/mihoyoClient.js
+        t = int(time.time())
+        a = "".join(random.sample('abcdefghijklmnopqrstuvwxyz0123456789', 6))
+        re = hashlib.md5(
+            f"salt=b253c83ab2609b1b600eddfe974df47b&t={t}&r={a}".encode(
+                encoding="utf-8")).hexdigest()
+        return f"{t},{a},{re}"
 
     def __init__(self, id: str) -> None:
+        """
+        针对每个目标商品进行初始化
+        >>> id: str # 商品ID(Good_ID)
+        """
         self.id = id
         self.result = None
-        self.req = None
+        self.req = requests.Session()
         self.url = "https://api-takumi.mihoyo.com/mall/v1/web/goods/exchange"
-        checkGame = "https://api-takumi.mihoyo.com/binding/api/getUserGameRolesByStoken"
+        getActionTicket = "https://api-takumi.mihoyo.com/auth/api/getActionTicketBySToken?action_type=game_role&stoken={stoken}&uid={bbs_uid}".format(
+            stoken=Good.stoken, bbs_uid=Good.bbs_uid)
+        checkGame = "https://api-takumi.mihoyo.com/binding/api/getUserGameRoles?point_sn=myb&action_ticket={actionTicket}&game_biz={game_biz}"
         checkGood = "https://api-takumi.mihoyo.com/mall/v1/web/goods/detail?app_id=1&point_sn=myb&goods_id={0}".format(
             self.id)
         self.data = {
@@ -95,22 +140,16 @@ class Good:
             "zh-CN,zh-Hans;q=0.9",
             "Connection":
             "keep-alive",
-            "Content-Length":
-            "88",
             "Content-Type":
             "application/json;charset=utf-8",
             "Cookie":
             Good.cookie,
             "Host":
             "api-takumi.mihoyo.com",
-            "Origin":
-            "https://webstatic.mihoyo.com",
-            "Referer":
-            "https://webstatic.mihoyo.com/",
             "User-Agent":
-            "Mozilla/5.0 (iPhone; CPU iPhone OS 15_1 like Mac OS X) AppleWebKit/605.1.15 (KHtimeL, like Gecko) miHoYoBBS/2.14.1",
+            USER_AGENT_EXCHANGE,
             "x-rpc-app_version":
-            "2.14.1",
+            X_RPC_APP_VERSION,
             "x-rpc-channel":
             "appstore",
             "x-rpc-client_type":
@@ -119,40 +158,68 @@ class Good:
             "".join(random.sample('abcdefghijklmnopqrstuvwxyz0123456789',
                                   32)).upper(),
             "x-rpc-device_model":
-            "iPhone10,2",
+            X_RPC_DEVICE_MODEL,
             "x-rpc-device_name":
             "".join(
                 random.sample('abcdefghijklmnopqrstuvwxyz0123456789',
                               random.randrange(5))).upper(),
             "x-rpc-sys_version":
-            "15.1"
+            X_RPC_SYS_VERSION
         }
 
         while True:
             try:
                 print(to_log("INFO", "正在检查商品：{0} 的详细信息".format(self.id)))
                 checkGood_data = json.loads(
-                    requests.get(checkGood, timeout=TIME_OUT).text)["data"]
+                    self.req.get(checkGood, timeout=TIME_OUT).text)["data"]
                 break
             except:
-                print(to_log("ERROR", "检查商品：{0} 服务器连接失败，正在重试".format(self.id)))
+                print(to_log("ERROR", traceback.format_exc()))
+                print(to_log("ERROR", "检查商品：{0} 失败，正在重试".format(self.id)))
+                continue
+
+        while True:
+            try:
+                print(to_log("INFO", "正在获取用户ActionTicket"))
+                getActionTicket_headers = self.headers.copy()
+                getActionTicket_headers[
+                    "User-Agent"] = USER_AGENT_GET_ACTION_TICKET
+                getActionTicket_headers.setdefault("DS", Good.get_DS())
+                getActionTicket_req = self.req.get(
+                    getActionTicket,
+                    headers=getActionTicket_headers,
+                    timeout=TIME_OUT)
+                actionTicket = json.loads(
+                    getActionTicket_req.text)["data"]["ticket"]
+                break
+            except:
+                print(to_log("ERROR", traceback.format_exc()))
+                print(to_log("ERROR", "获取用户ActionTicket失败，正在重试"))
                 continue
 
         if checkGood_data["type"] == 2:
+            if "stoken" not in Good.cookie:
+                print(
+                    to_log(
+                        "ERROR",
+                        "商品：{0} 为游戏内物品，由于未配置 stoken，放弃兑换该商品".format(self.id)))
+                self.result = -1
+
             game_biz = checkGood_data["game_biz"]
             while True:
                 try:
                     print(to_log("INFO",
                                  "正在检查游戏账户：{0} 的详细信息".format(Good.uid)))
                     user_list = json.loads(
-                        requests.get(checkGame,
+                        self.req.get(checkGame.format(
+                            actionTicket=actionTicket, game_biz=game_biz),
                                      headers=self.headers,
                                      timeout=TIME_OUT).text)["data"]["list"]
                     break
                 except:
+                    print(to_log("ERROR", traceback.format_exc()))
                     print(
-                        to_log("ERROR",
-                               "检查游戏账户：{0} 服务器连接失败，正在重试".format(Good.uid)))
+                        to_log("ERROR", "检查游戏账户：{0} 失败，正在重试".format(Good.uid)))
                     continue
             for user in user_list:
                 if user["game_biz"] == game_biz and user[
@@ -161,7 +228,14 @@ class Good:
                     self.data.setdefault("region", user["region"])
                     self.data.setdefault("game_biz", game_biz)
 
+        self.headers.setdefault("Content-Length", "88")
+
     def start(self) -> None:
+        """
+        执行兑换操作
+        """
+        if self.result == -1:
+            print(to_log("WARN", "商品：{0} 未初始化完成，放弃兑换".format(self.id)))
         self.req = requests.Session()
         while True:
             try:
@@ -169,7 +243,8 @@ class Good:
                                             headers=self.headers,
                                             json=self.data)
             except:
-                print(to_log("INFO", "兑换商品：{0} 服务器连接失败，正在重试".format(self.id)))
+                print(to_log("ERROR", traceback.format_exc()))
+                print(to_log("ERROR", "兑换商品：{0} 失败，正在重试".format(self.id)))
                 continue
             print(
                 to_log(
@@ -192,11 +267,13 @@ for id in good_list:
     queue.append(Good(id))
 
 
-## 检查网络连接和显示剩余时间
 class CheckNetwork:
+    """
+    检查网络连接和显示剩余时间
+    """
     global conf
 
-    timeUp_Str = conf.get("Config", "Time")
+    timeUp_Str = conf.get("Config", "Time")  # 获取配置文件中的兑换开始时间
     checkTime = conf.get("Preference", "Check_Time")  # 每隔多久检查一次网络连接情况
     stopCheck = conf.get("Preference", "Stop_Check")  # 距离开始兑换还剩多久停止检查网络
     isCheck = conf.get("Preference", "Check_Network")  # 是否自动检测网络连接情况
@@ -226,7 +303,7 @@ class CheckNetwork:
                 CheckNetwork.lastCheck = time.time()
                 print("\n")
                 if CheckNetwork.result == None:
-                    print(to_log("ERROR", "检测到网络连接异常！\n"))
+                    print(to_log("WARN", "检测到网络连接异常！\n"))
                 else:
                     CheckNetwork.result = CheckNetwork.result * 1000
                     print(
@@ -235,8 +312,11 @@ class CheckNetwork:
                                 round(CheckNetwork.result, 2))))
 
 
-# 时间戳转字符串时间（无传入参数则返回当前时间）
 def timeStampToStr(timeStamp: float = None) -> str:
+    """
+    时间戳转字符串时间（无传入参数则返回当前时间）
+    >>> timeStamp: float # 时间戳
+    """
     return time.strftime("%H:%M:%S", time.localtime(timeStamp))
 
 
