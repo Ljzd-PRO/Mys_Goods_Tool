8d87abaeba599934ca1dee5eb6ced36e2d96c20f
Ljzd-PRO <mcdhacg@foxmail.com>
2023-05-03 03:15:13 +0800

    完成入口代码

--- __init__.py
+++ __init__.py
@@ -1,9 +1,7 @@
 import multiprocessing
 
 from mys_goods_tool.__main__ import main
-from mys_goods_tool.tui import TuiApp
 
 if __name__ == "__main__":
     multiprocessing.freeze_support()
-    app = TuiApp()
-    main(app)
+    main()
--- mys_goods_tool/__main__.py
+++ mys_goods_tool/__main__.py
@@ -7,25 +7,6 @@ import mys_goods_tool.user_data
 from mys_goods_tool.exchange_mode import exchange_mode_simple
 from mys_goods_tool.user_data import load_config
 
-VERSION = "v2.0.0-dev"
-"""程序当前版本"""
-
-
-def main(textual_app: Optional[App]):
-    arg = arg_parser.parse_args()
-
-    if arg.conf is not None:
-        mys_goods_tool.user_data.CONFIG_PATH = arg.conf
-        mys_goods_tool.user_data.config = load_config()
-    if arg.mode == "guide":
-        textual_app.run()
-    elif arg.mode == "exchange":
-        # TODO 兑换模式
-        ...
-    elif arg.mode == "exchange-simple":
-        exchange_mode_simple()
-
-
 USAGE = """
 Mys_Goods_Tool
 使用说明：
@@ -33,17 +14,14 @@ Mys_Goods_Tool
 选项：
     -h, --help 显示此帮助信息
     -m, --mode <参数> 指定运行模式
-        guide 指引模式（默认）
-        exchange 兑换模式，等待到达兑换时间并自动兑换
+        guide TUI指引模式，包含登陆绑定、管理兑换计划和开始兑换等功能（默认）
         exchange-simple 兑换模式，无TUI界面，仅输出日志文本
     -c, --conf <参数> 指定用户数据文件路径
 例如：
-    %(prog)s -m exchange -c ./workplace/user_data.json
-        通过该命令运行本程序，将读取 ./workplace/user_data.json 用户数据文件，并直接进入兑换模式，等待到达兑换时间并执行兑换。
-    %(prog)s -m test
-        通过该命令运行本程序，将读取程序目录下的用户数据文件user_data.json，检查用户数据文件格式是否正确，并直接尝试兑换以测试是否为未来的兑换计划做好准备。
+    %(prog)s -m exchange-simple -c ./workplace/user_data.json
+        通过该命令运行本程序，将读取 ./workplace/user_data.json 用户数据文件，并直接进入无TUI界面的兑换模式，等待到达兑换时间并执行兑换。
     %(prog)s
-        通过该命令运行本程序或直接双击打开程序，将读取程序目录下的用户数据文件user_data.json，并提供添加兑换计划、查看兑换计划、删除兑换计划、设置收货地址等功能。
+        通过该命令运行本程序或直接双击打开程序，将读取程序目录下的用户数据文件user_data.json，并提供登录绑定、管理兑换计划等功能。
         """.strip()
 
 
@@ -54,11 +32,27 @@ class ArgumentParserWithHelp(ArgumentParser):
 
 
 arg_parser = ArgumentParserWithHelp(description="Mys_Goods_Tool", usage=USAGE)
-arg_parser.add_argument("-m", "--mode", dest="mode", choices=["guide", "exchange", "exchange-simple"],
+arg_parser.add_argument("-m", "--mode", dest="mode", choices=["guide", "exchange-simple"],
                         default="guide")
 arg_parser.add_argument("-c", "--conf", type=str, dest="conf", default=None)
 
 TEXTUAL_DEBUG = False
+
+
+def main(textual_app: Optional[App] = None):
+    arg = arg_parser.parse_args()
+
+    if arg.conf is not None:
+        mys_goods_tool.user_data.CONFIG_PATH = arg.conf
+        mys_goods_tool.user_data.config = load_config()
+    if arg.mode == "guide":
+        if textual_app is None:
+            from mys_goods_tool.tui import TuiApp
+            textual_app = TuiApp()
+        textual_app.run()
+    elif arg.mode == "exchange-simple":
+        exchange_mode_simple()
+
 
 if __name__ == "__main__":
     app: Optional[App] = None
