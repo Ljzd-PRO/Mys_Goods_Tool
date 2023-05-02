6f6c99ba0aca0760135c44550ccc87859a17d067
Ljzd-PRO <mcdhacg@foxmail.com>
2023-05-03 02:54:33 +0800

    更改兑换线程数为2

--- mys_goods_tool/user_data.py
+++ mys_goods_tool/user_data.py
@@ -294,7 +294,7 @@ class Preference(BaseSettings):
     """GEETEST行为验证 网站静态文件目录（默认读取本地包自带的静态文件）"""
     geetest_listen_address: Optional[Tuple[str, int]] = ("localhost", 0)
     """登录时使用的 GEETEST行为验证 WEB服务 本地监听地址"""
-    exchange_thread_count: int = 3
+    exchange_thread_count: int = 2
     """兑换线程数"""
     exchange_latency: Tuple[float, float] = (0, 0.35)
     """兑换时间延迟随机范围（单位：秒）（防止因为发出请求的时间过于精准而被服务器认定为非人工操作）"""
